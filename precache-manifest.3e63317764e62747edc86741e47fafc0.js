748d6f686e95b578619ce81061e6544c33ce67f9
Amanda Gama <amgama17@gmail.com>
2020-06-01 00:13:25 -0300

    Updates

---
+++ precache-manifest.3e63317764e62747edc86741e47fafc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57aabed54eeda37146c076b71070cb3",
+    "url": "/frontend-challenge/index.html"
+  },
+  {
+    "revision": "cbc8bd0e7c455d971226",
+    "url": "/frontend-challenge/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "73da1b78c43b6eb9c2da",
+    "url": "/frontend-challenge/static/js/2.d718557e.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/frontend-challenge/static/js/2.d718557e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbc8bd0e7c455d971226",
+    "url": "/frontend-challenge/static/js/main.01867e13.chunk.js"
+  },
+  {
+    "revision": "9e98e83ee7a5468d6c9e",
+    "url": "/frontend-challenge/static/js/runtime-main.5218adef.js"
+  }
+]);
